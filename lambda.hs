e58c536c40fd1525669577f840d28ed025a80d5d
Kamila Souckova <kamila@vesmir.sk>
2014-03-10 14:22:11 +0100

    change text width to 100

--- lambda.hs
+++ lambda.hs
@@ -9,13 +9,12 @@ type Name = String
 data LExp = APPL LExp LExp   -- application
           | IDENT Name       -- variable
           | LAMBDA Name LExp -- abstraction
-          -- I will add the following once I find out how my functions should
-          -- handle those
+          -- I will add the following once I find out how my functions should handle those
           -- | SCONST Name      -- string constant (built-in function, ...)
           -- | CONST Integer    -- numeric constant
     deriving (Show, Read, Eq)
 
--------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
 
 render :: LExp -> String
 render (IDENT x)    = x
@@ -25,6 +24,7 @@ render (APPL m n)   = "("++(render m)++" "++(render n)++")"
 prender exp = putStrLn $ render exp
 
 -- it is a monad, but I have no idea what I am talking about... TODO
+-- also TODO: if I move the ' ' check to IDENT, brackets around application will not be needed
 parse :: String -> (LExp, String)
 parse ('(':xs)
     | head rest == ')' = (m, tail rest)
@@ -41,7 +41,7 @@ cparse s
     | otherwise        = error "Parse error: not a single, complete expression"
     where parsed = parse s
 
--------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
 
 free :: LExp -> [Name]
 free (IDENT x)    = [x]
@@ -53,7 +53,7 @@ subt (IDENT x)    = [IDENT x]
 subt (LAMBDA x m) = (subt m) `union` [(LAMBDA x m)]
 subt (APPL m n)   = (subt m) `union` (subt n) `union` [(APPL m n)]
 
--------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
 
 k = cparse "λx.λy.x"
 i = cparse "λx.λy"
